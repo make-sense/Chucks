8f2ad1ee2cd4d1e88bb17d258a63783bfa91e64f
Donghoon Lee <movehoon@gmail.com>
2015-06-30 20:57:02 +0900

    Drag chuck

--- Assets/Scripts/Program.cs
+++ Assets/Scripts/Program.cs
@@ -40,6 +40,9 @@ public class Program : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown(KeyCode.Escape)) 
+		{
+			Application.Quit(); 
+		}
 	}
 }
