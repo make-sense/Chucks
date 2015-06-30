59519bb5ff2fee25119b5fa8bf9eda1a85c55904
Donghoon Lee <movehoon@gmail.com>
2015-06-30 20:22:05 +0900

    Change panel to Show/Hide

--- Assets/Scripts/Program.cs
+++ Assets/Scripts/Program.cs
@@ -3,9 +3,39 @@ using System.Collections;
 
 public class Program : MonoBehaviour {
 
+	public GameObject ActorPanel;
+	public GameObject ChuckPanel;
+
+	public void ActorPanel_Show () {
+		ActorPanel.SetActive (true);
+	}
+	public void ActorPanel_Hide () {
+		ActorPanel.SetActive (false);
+	}
+	public void ActorPanel_Toggle () {
+		if (ActorPanel.activeSelf)
+			ActorPanel.SetActive (false);
+		else
+			ActorPanel.SetActive (true);
+	}
+
+	public void ChuckPanel_Show () {
+		ChuckPanel.SetActive (true);
+	}
+	public void ChuckPanel_Hide () {
+		ChuckPanel.SetActive (false);
+	}
+	public void ChuckPanel_Toggle () {
+		if (ChuckPanel.activeSelf)
+			ChuckPanel.SetActive (false);
+		else
+			ChuckPanel.SetActive (true);
+	}
+
 	// Use this for initialization
 	void Start () {
-	
+		ActorPanel_Hide ();
+		ChuckPanel_Hide ();
 	}
 	
 	// Update is called once per frame
